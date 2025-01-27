e74cddddecb19ba3f36ba8427d4239929d68382a
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-01-27 16:16:15 +0000

    salient java points

--- MyJavaCodes.java
+++ MyJavaCodes.java
@@ -1,3 +1,5 @@
+// Java Note points
+
 public class MyJavaCodes {   //public is an access modifier || class is a keyword used to declare a class in java.
     public static void main(String[] args) { //main method || String[] args allows you to pass commmand line arguments to the prog
         System.out.println("It Works."); // System's standard output || println is a method used to print a line of text.
