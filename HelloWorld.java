afb452e8bcee2d91c25e1b3db0cdc779f4efb978
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-02-04 12:05:39 +0000

    displaying  a simple welcome message in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
-public class HelloWorld {
+// first java program which displays "hello world !"
+
+//define the helloworld class - should match the  filename
+// In java, all codes must be in a class
+public class HelloWorld { 
+//the main method is   the entry point of the program, is where execution starts
+// public - makes the method accessible from anywhere
+//static - means the method belongs to the class, not an instance of the class
+//void - means the method returns no value
     public static void main (String[] args) {
-        System.out.println("Hello World, this is java!");
+        System.out.println("Hello World, this is java!"); // displays a message
     }
 }
+
